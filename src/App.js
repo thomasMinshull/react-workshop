6132eb755b6823234ed3e066dc7aa6a2b24210ff
TMin <email": null>
2017-05-22 20:12:40 -0700

    step 2 complete

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
         </div>
 
         <div className="App-intro">
-          Hello, <Name />
+          Hello, <Name class='inline' name='Tom'/>
         </div>
 
       </div>
